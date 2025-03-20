c0a19f1e271356409560f55a8c9df44326b29682
Roman Pena <rpena003@odu.edu>
2025-03-20 12:35:11 -0400

    Added Calendar display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import axios from "axios";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,8 +16,31 @@ function App() {
     fetchAPI();
   });
 
+  const handleEventClick = (clickInfo) => {
+    alert(clickInfo.event.title + ' Copied to Clipboard');
+    navigator.clipboard.writeText(clickInfo.event.title);
+  }
+
   return (
     <>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left:'prev, next, today',
+          center: 'title',
+          right: 'dayGridMonth'
+        }}
+        events={[
+          { title: 'event 3', date: '2025-03-10' },
+          { title: 'event 2', date: '2025-03-27' },
+          { title: 'event 1', date: '2025-03-28' },
+        ]}
+
+        editable={true}
+
+        eventClick={handleEventClick}
+      />
       <div>
       </div>
       <p className="read-the-docs">
